3fdb9c0255718a48b1692922ff72cbcb2ae058f9
Amirhossein Bargharari <60382663+AmirHBrgh@users.noreply.github.com>
2021-07-18 11:24:59 +0430

    2_5 completed

---
+++ Code/Section2_5.m
@@ -0,0 +1,6 @@
+addpath('C:\Workspace\Nueroscience\Code\MatlabFunctions\tview');
+cd Data\Spike_and_Log_Files\000412.a01
+tview('000412.a01atune.log');
+cd ../000524.c01
+tview('000524.c01atune.log');
+cd ..\..\..
