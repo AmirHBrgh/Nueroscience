44e11dc6a3f325be01f1641148cb06d83df7f582
tandalalam <maziar56bnd@gmail.com>
2021-07-18 13:11:59 +0430

    minor change

--- Code/Variance.m
+++ Code/Variance.m
@@ -1,6 +1,7 @@
 %%
 clear, clc
-load Data/neurons_full_struct.mat
+load v.mat
+load('Data/neurons_full_struct.mat')
 %% Test Sta
 sta_result = [];
 stc_result = [];
@@ -21,21 +22,26 @@ for i = 1:length(neurons)
     
     sta_result(i) = vartest2(zeta_sta, random_zeta_sta, 'Tail', 'right');
     
-    zeta_stc = reshape(v(:,i),1,256)*spike_trigerred;
-    random_zeta_stc = reshape(v(:,i),1,256)*random_spike;
+    v1 = neuron.v1;
+    zeta_stc = reshape(v1,1,256)*spike_trigerred;
+    random_zeta_stc = reshape(v1,1,256)*random_spike;
     
     stc_result(i) = vartest2(zeta_stc, random_zeta_stc, 'Tail', 'right');
 end
 %%
-zeta4 = reshape(sta,1,256)*spike_trigerred;
-
+fprintf('mean result for Spike triggered correlation:%d\n', mean(stc_result));
+fprintf('mean result for Spike triggered correlation:%d\n', mean(sta_result));
 %%
-[h, p] = vartest2(zeta4, random_zeta4);
-%%
-v1 = neuron.v1;
-v2 = neuron.v2;
-v3 = neuron.v3;
-%%
+neuron = neurons(10);
+freq = 59.721395;
+% Spike triggered ensemble
+neuron_code = neuron.outs(1).hdr.DataInfo.DataFrom(1:10);
+[sta, spike_trigerred] = Func_FindSTA(neuron);
+spike_trigerred = reshape(spike_trigerred,256,length(spike_trigerred));
+%control stimuli ensemble
+random_events = 10000*rand(1,(length(spike_trigerred)))*(32767/freq);
+random_spike = Func_StimuliExtraction (random_events);
+random_spike = reshape(random_spike, 256, length(random_spike));
 for i = 1:256
 zeta = v(:,I(i))' * spike_trigerred;
 random_zeta = v(:,I(i))' *random_spike;
@@ -43,22 +49,3 @@ random_zeta = v(:,I(i))' *random_spike;
 end
 
 plot(p,'Marker','o','LineWidth',1);
-
-
-
-
-%zeta2 = reshape(v2,1,256)*spike_trigerred;
-%random_zeta2 = reshape(v2,1,256)*random_spike;
-
-%zeta3 = reshape(v3,1,256)*spike_trigerred;
-%random_zeta3 = reshape(v3,1,256)*random_spike;
-%%
-histogram(zeta,'Normalization', 'pdf', 'BinMethod','fd')
-hold on
-histogram(random_zeta,'Normalization', 'pdf', 'BinMethod','fd') 
-%%
-[h, p(1)] = vartest2(zeta1, random_zeta1);
-[h, p(2)] = vartest2(zeta2, random_zeta2);
-[h, p(3)] = vartest2(zeta3, random_zeta3);
-%%
-plot(p,'r','LineWidth',1,'Marker','o');
